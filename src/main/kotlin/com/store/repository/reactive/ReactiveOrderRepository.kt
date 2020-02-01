a533a447fb97ed9b652bc3a61c3782fcc640badc
Jiwhun Kim <jiwhun.kim@outlook.com>
2020-02-01 16:07:17 +0900

    reacitve api 추가

--- src/main/kotlin/com/store/repository/reactive/ReactiveOrderRepository.kt
+++ src/main/kotlin/com/store/repository/reactive/ReactiveOrderRepository.kt
@@ -3,10 +3,8 @@ package com.store.repository.reactive
 import com.store.domain.Order
 import org.springframework.data.r2dbc.repository.R2dbcRepository
 import org.springframework.data.r2dbc.repository.query.Query
-import org.springframework.stereotype.Repository
 import reactor.core.publisher.Mono
 
-@Repository
 interface ReactiveOrderRepository : R2dbcRepository<Order, Long> {
     @Query("SELECT * from orders where id = :id")
     fun findByIdRx(id: Long): Mono<Order>
